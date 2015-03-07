b4e642f6fbe51f36b5adededdae6650cf3b5ad01
youchan <youchan01@gmail.com>
2015-03-07 16:49:59 +0900

    Arrows :cupid:

--- client/app/filter/region_name.js
+++ client/app/filter/region_name.js
@@ -3,14 +3,10 @@ import { REGIONS_URL } from 'app/urls';
 export function regionNameFilter($http) {
   var region = [];
   $http.get(REGIONS_URL)
-    .success(function(data) {
-      region = data;
-    });
-  return function(input) {
-    var ret = '';
-    angular.forEach(region, function(v) {
-      if (v.id === input) ret = v.name;
-    });
-    return ret;
-  };
+    .success((data) => region = data)
+  return (input) => {
+    var ret = ''
+    angular.forEach(region, (v) => { if (v.id === input) ret = v.name })
+    return ret
+  }
 }

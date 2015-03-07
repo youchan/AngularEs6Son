bca516efa2542bf783e15bbba64f638aa0df5bfe
tsuchikazu <huneyhunt77@gmail.com>
2015-03-07 16:26:53 +0900

    新規登録ボタンが表示されるテスト追加

--- e2e/list.spec.js
+++ e2e/list.spec.js
@@ -1,8 +1,23 @@
 describe('一覧ページ', ()=> {
+  beforeEach(function() {
+    browser.get('http://localhost:9000/');
+  });
+
+  describe('新規登録', ()=> {
+    it('新規登録ボタンが表示されること', (done)=> {
+      var createButton = element(by.css('h2 .btn-warning'));
+      browser.wait(()=>{
+        return createButton.isPresent();
+      }, 10000).then(()=>{
+        expect(createButton.getText()).toBe("新規登録")
+        done();
+      });
+    });
+  });
+
   describe('一覧', ()=> {
     var firstBean;
     beforeEach(function(done) {
-      browser.get('http://localhost:9000/');
       firstBean = element(by.repeater('bean in list.beans').row(0));
       browser.wait(()=>{
         return firstBean.isPresent();
@@ -20,7 +35,7 @@ describe('一覧ページ', ()=> {
 
     it('一覧のボタンが表示されること', ()=> {
       expect(firstBean.element(by.css('.btn-primary')).getText()).toContain("編集");
-      expect(firstBean.element(by.css('.btn-danger')).getText()).toEqual("削除");
+      expect(firstBean.element(by.css('.btn-danger')).getText()).toBe("削除");
     });
   });
 });
